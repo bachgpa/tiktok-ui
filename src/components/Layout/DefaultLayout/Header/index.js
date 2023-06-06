baf62f16e4aaad8d1b406abacd39f571f97d915f
Bách Nguyễn <bachgpa@gmail.com>
2023-06-07 01:12:39 +0700

    add button and search

--- src/components/Layout/DefaultLayout/Header/index.js
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -1,12 +1,32 @@
 import classNames from 'classnames/bind';
-import styles from './Header.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
+import images from '../../../../assets/images';
+import styles from './Header.module.scss';
 const cx = classNames.bind(styles);
 
 function Header() {
     return (
         <div className={cx(styles.wrapper)}>
-            <div className={cx('inner')}>content of header</div>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="tiktok" />
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder="Tìm kiêsm" spellCheck={false} />
+                    <button>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />{' '}
+                    </button>
+                </div>
+                <div className={cx('action')}>
+                    <button>{/* {login} */}</button>
+                </div>
+            </div>
         </div>
     );
 }
