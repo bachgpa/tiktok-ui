c2d5714e202b57d3f5730fc49bd4d97b50af7b6b
Bách Nguyễn <105241006+bachgpa@users.noreply.github.com>
2023-06-21 23:21:15 +0700

    cant fix tippy, add button logic&css
    fixing tippy fail,
    add button logic and css

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -9,10 +9,16 @@ function Button({
     href,
     primary = false,
     outline = false,
-    small,
-    medium,
-    large,
+    rounded = false,
+    text = false,
+    disabled = false,
+    small = false,
+    medium = false,
+    large = false,
+    className,
     children,
+    leftIcon,
+    rightIcon,
     onClick,
     ...passProps
 }) {
@@ -23,6 +29,16 @@ function Button({
         onClick,
         ...passProps,
     };
+
+    //Gỡ sự kiện nếu nút disabled (CÁI NÀY CẦN XEM LẠI VÌ CHƯA HIỂU)
+    if (disabled) {
+        Object.keys(props).forEach((key) => {
+            if (key.startsWith('on') && typeof props[key] === 'function') {
+                delete props[key];
+            }
+        });
+    }
+
     //logic nếu có thuộc tính nào đó
     if (to) {
         props.to = to;
@@ -33,16 +49,23 @@ function Button({
     }
 
     const classes = cx('wrapper', {
+        // className dùng để nhận diện classNames{cx('...')} được truyền vào từ Header file
+        [className]: className,
         primary,
         outline,
+        rounded,
+        text,
         small,
         medium,
         large,
+        disabled,
     });
 
     return (
         <Comp className={classes} {...props}>
-            <span>{children}</span>
+            {leftIcon && <span className={cx('icon')}>{leftIcon}</span>}
+            <span className={cx('title')}>{children}</span>
+            {rightIcon && <span className={cx('icon')}>{rightIcon}</span>}
         </Comp>
     );
 }
--- src/components/Layout/DefaultLayout/Header/index.js
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -29,49 +29,52 @@ function Header() {
         <div className={cx(styles.wrapper)}>
             <div className={cx('inner')}>
                 <div className={cx('logo')}></div>
-                <Tippy
-                    interactive
-                    visible={searchResult.length > 0}
-                    render={(attrs) => (
-                        <div
-                            className={cx('search-result')}
-                            tabIndex="-1"
-                            {...attrs}
-                        >
-                            <PopperWrapper>
-                                <h4 className={cx('search-title')}>Accounts</h4>
-                                <AccountItem />
-                                <AccountItem />
-                                <AccountItem />
-                                <AccountItem />
-                                <div className={cx('more-result')}>
-                                    Xem tất cả kết quả cho " "
-                                </div>
-                            </PopperWrapper>
-                        </div>
-                    )}
-                >
-                    <div className={cx('search')}>
-                        <input placeholder="Tìm kiếm" spellCheck={false} />
-                        <button className={cx('delete')}>
-                            <FontAwesomeIcon icon={faCircleXmark} />
-                        </button>
-                        <FontAwesomeIcon
-                            className={cx('loading')}
-                            icon={faSpinner}
-                        />
+                <div className={cx('Tippy')}>
+                    <Tippy
+                        interactive
+                        visible={searchResult.length > 0}
+                        render={(attrs) => (
+                            <div
+                                className={cx('search-result')}
+                                tabIndex="-1"
+                                {...attrs}
+                            >
+                                <PopperWrapper>
+                                    <h4 className={cx('search-title')}>
+                                        Accounts
+                                    </h4>
+                                    <AccountItem />
+                                    <AccountItem />
+                                    <AccountItem />
+                                    <AccountItem />
+                                    <div className={cx('more-result')}>
+                                        Xem tất cả kết quả cho " "
+                                    </div>
+                                </PopperWrapper>
+                            </div>
+                        )}
+                    >
+                        <div className={cx('search')}>
+                            <input placeholder="Tìm kiếm" spellCheck={false} />
+                            <button className={cx('delete')}>
+                                <FontAwesomeIcon icon={faCircleXmark} />
+                            </button>
+                            <FontAwesomeIcon
+                                className={cx('loading')}
+                                icon={faSpinner}
+                            />
 
-                        <button className={cx('search-btn')}>
-                            <FontAwesomeIcon icon={faMagnifyingGlass} />{' '}
-                        </button>
-                    </div>
-                </Tippy>
+                            <button className={cx('search-btn')}>
+                                <FontAwesomeIcon icon={faMagnifyingGlass} />{' '}
+                            </button>
+                        </div>
+                    </Tippy>
+                </div>
                 <div className={cx('action')}>
-                    {/* <div className={cx('upload')}>
-                        <FontAwesomeIcon icon={faPlus} />
-                        <p>Tải lên</p>
-                    </div> */}
-                    <Button outline>Login</Button>
+                    <Button text leftIcon={<FontAwesomeIcon icon={faPlus} />}>
+                        <span>Upload</span>
+                    </Button>
+                    <Button primary>Login</Button>
                 </div>
             </div>
         </div>
